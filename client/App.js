894ef2cc0da14073d02e492ffb0381ab3df65c45
dvebino <dhuang2495@gmail.com>
2022-06-30 16:24:01 -0400

    query not working

--- client/App.js
+++ client/App.js
@@ -2,35 +2,39 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { AppRegistry } from "react-native";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
 import { gql } from "apollo-boost";
+import Data from './Data';
 
 const client = new ApolloClient({
   uri: "localhost:8080/graphql",
   cache: new InMemoryCache(),
 });
 
-const query = gql`
-{
-  movies {
-    title
-  }
-}
-`;
+// const query = gql`
+// {
+//   movies {
+//     title
+//   }
+// }
+// `;
 
-const Movies = ({ data }) => {
-<View>
-  <Text>Title: {data.movies && data.movies.title}</Text>
-</View>;
-};
+// const Movies = () => {
+//   const {loading, error, data} = useQuery(query);
+//   console.log(data)
+//   return(
+//     <View>
+//       <Text>Title: {data.movies && data.movies.title}</Text>
+//     </View>
+//   )
+// };
 
 export default function App() {
-  console.log(query)
-
 
   return (
     <ApolloProvider client={client}>
       <View style={styles.container}>
+        <Data />
         <Text style={{ fontWeight: "bold", fontSize: 30 }}>Data Goes Here</Text>
         <StatusBar style="auto" />
       </View>
--- client/Data.js
+++ client/Data.js
@@ -2,10 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { AppRegistry } from 'react-native';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client';
+import { gql } from "apollo-boost";
 
-const DATA = gql`{
-  movies {
+const GET_MOVIES = gql`
+query {
+  movies (first: 10){
     title
   }
-  }`
+}
+`;
+
+export default () => {
+  const {loading, error, data} = useQuery(`{movies {title}}`);
+  console.log(error)
+  return(
+    <View>
+      {/* <Text>Title: {data.movies && data.movies.title}</Text> */}
+      <Text>Hi</Text>
+    </View>
+  )
+};
