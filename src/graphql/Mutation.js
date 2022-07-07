a83ec333a6be6b9e0912f96d452a899bb087ea35
LZHershey <laurenzhershey@gmail.com>
2022-07-07 08:56:21 -0700

    genre mutations:

--- src/graphql/Mutation.js
+++ src/graphql/Mutation.js
@@ -2,24 +2,22 @@ import { gql } from '@apollo/client';
 
 export const ADD_GENRE_MUTATION = gql`
   mutation addGenre {
-    addGenre {
-      action
-      animation
-      comedy
-      crime
-      documentation
-      drama
-      european
-      family
-      fantasy
-      history
-      horror
-      music
-      romance
-      scifi
-      thriller
-      war
-      western
-    }
+    action
+    animation
+    comedy
+    crime
+    documentation
+    drama
+    european
+    family
+    fantasy
+    history
+    horror
+    music
+    romance
+    scifi
+    thriller
+    war
+    western
   }
 `;
