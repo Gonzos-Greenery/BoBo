b10e759373aa2c4a1245142f1341b8ac04ec93cf
alli-g <acgokbudak@gmail.com>
2022-07-12 15:19:58 -0600

    seed files and associations created

---
+++ script/partyRatingSeed.js
@@ -0,0 +1,29 @@
+const PartyRatingSeed = [
+  {
+    rating: 1,
+    userId: 1,
+    movieId: 1,
+    partyId: 1,
+  },
+
+  {
+    rating: 3,
+    userId: 2,
+    movieId: 1,
+    partyId: 1,
+  },
+  {
+    rating: 2,
+    userId: 3,
+    movieId: 1,
+    partyId: 1,
+  },
+  {
+    rating: 2,
+    userId: 4,
+    movieId: 1,
+    partyId: 1,
+  },
+];
+
+module.exports = PartyRatingSeed;
